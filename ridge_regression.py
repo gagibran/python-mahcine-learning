09bc5776dfbfeb19d1413d0c69cd3ecdb524c824
Gabriel Alvares Gibran <gagibran@gmail.com>
2020-11-28 20:19:35 -0300

    Reorganized the directories

--- ridge_regression.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-
-# Imports:
-import pandas as pd
---
+++ ridge_regression/ridge_regression.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Created by Gabriel Alvares Gibran on 11/28/2020.
+'''
+'''
+
+# Imports:
+import pandas as pd
